c386806e548315480cc5afd8c602cbbad6cd5286
Jacoe Guo <jacoesad@126.com>
2025-11-25 23:24:12 +0800

    C03: 3.4 change project structure
    - 重构代码格式

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use zero2prod::run;
 
-async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().finish()
-}
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    run().await
 }
